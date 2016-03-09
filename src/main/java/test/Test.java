b44334bc31be7e4fce2fb2bde8a482214ecf6c8a
tprift001c <Tomi_Prifti@cable.comcast.com>
2016-03-09 13:50:42 -0500

    Test 1

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -12,6 +12,7 @@ import org.joda.time.format.DateTimeFormatter;
 public class Test {
 	//test
 	public static void main(String[] args) {
+		
     	DateTime current = new DateTime(DateTimeZone.UTC);
     	String dob = "2016-03-8T22:19:47.000Z";
     	DateTimeFormatter f = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
